af567d7d2ef4f48d8632eefa234e21aef5973bb6
Kelu <kelu124@gmail.com>
2018-02-08 23:44:37 +0100

    bad commit

--- CreateArchives.py
+++ CreateArchives.py
@@ -4,23 +4,26 @@ import numpy as np
 import codecs, json 
 import time
 
-try:
-    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tso.set_keywords(['ice40']) # let's define all words we would like to have a look for
-    tso.set_include_entities(False) # and don't give us all those entity information
-    
-    tmp = []
-     # this is where the fun actually starts :)
-        
-    for tweet in ts.search_tweets_iterable(tso):
-        print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
-        tmp.append(tweet)
-        
-except TwitterSearchException as e: # take care of all those ugly errors if there are some
-    print(e)
+KW = ["ultraplus", "icecube2", "oe1cxw", "ice40", "icoBoard", "iceZero", "BlackIce", "TinyFPGA", "BeagleWire", "Icestorm","upduino","icoprog","1Bitsy","fpgawars"]
 
-tstamp = time.time()
-A = ts.get_tweets()
+for kw in KW:
+	try:
+	    tso = TwitterSearchOrder() # create a TwitterSearchOrder object
+	    tso.set_keywords([kw],False) # let's define all words we would like to have a look for
+	    tso.set_include_entities(False) # and don't give us all those entity information
+	    
+	    tmp = []
+	     # this is where the fun actually starts :)
+		
+	    for tweet in ts.search_tweets_iterable(tso):
+		#print( '@%s tweeted: %s' % ( tweet['user']['screen_name'], tweet['text'] ) )
+		tmp.append(tweet)
+		
+	except TwitterSearchException as e: # take care of all those ugly errors if there are some
+	    print(e)
 
-with open(str(tstamp)+".json", 'w') as outfile:
-    json.dump(A, outfile)
+	tstamp = time.time()
+	A = ts.get_tweets()
+
+	with open(str(tstamp)+"-"+kw+".json", 'w') as outfile:
+	    json.dump(A, outfile)
