c05017dc750775950c68082eba16f59b7bf51da9
Kelu <kelu124@gmail.com>
2018-04-01 18:30:08 +0200

    New tweets

--- CreateArchives.py
+++ CreateArchives.py
@@ -4,7 +4,7 @@ import numpy as np
 import codecs, json 
 import time
 
-KW = ["lattice ultra plus","arachne-pr","fpga_dave","ultraplus", "icecube2", "oe1cxw", "ice40", "icoBoard", "iceZero", "BlackIce", "TinyFPGA", "BeagleWire", "Icestorm","upduino","icoprog","1Bitsy","fpgawars"]
+KW = ["lattice ultra plus","arachne-pr","fpga_dave","ultraplus", "icecube2", "oe1cxw", "ice40", "icezum", "icoBoard", "iceZero", "BlackIce", "TinyFPGA", "BeagleWire", "Icestorm","upduino","icoprog","1Bitsy","fpgawars"]
 
 for kw in KW:
 	try:
